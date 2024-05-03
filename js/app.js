6a1633c11f94e7d347eeb4475f364a3d3dabe377
vsevolodstudent <168846807+vsevolodstudent@users.noreply.github.com>
2024-05-03 18:17:12 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,134 @@
+(() => {
+    "use strict";
+    let bodyLockStatus = true;
+    let bodyLockToggle = (delay = 500) => {
+        if (document.documentElement.classList.contains("lock")) bodyUnlock(delay); else bodyLock(delay);
+    };
+    let bodyUnlock = (delay = 500) => {
+        if (bodyLockStatus) {
+            const lockPaddingElements = document.querySelectorAll("[data-lp]");
+            setTimeout((() => {
+                lockPaddingElements.forEach((lockPaddingElement => {
+                    lockPaddingElement.style.paddingRight = "";
+                }));
+                document.body.style.paddingRight = "";
+                document.documentElement.classList.remove("lock");
+            }), delay);
+            bodyLockStatus = false;
+            setTimeout((function() {
+                bodyLockStatus = true;
+            }), delay);
+        }
+    };
+    let bodyLock = (delay = 500) => {
+        if (bodyLockStatus) {
+            const lockPaddingElements = document.querySelectorAll("[data-lp]");
+            const lockPaddingValue = window.innerWidth - document.body.offsetWidth + "px";
+            lockPaddingElements.forEach((lockPaddingElement => {
+                lockPaddingElement.style.paddingRight = lockPaddingValue;
+            }));
+            document.body.style.paddingRight = lockPaddingValue;
+            document.documentElement.classList.add("lock");
+            bodyLockStatus = false;
+            setTimeout((function() {
+                bodyLockStatus = true;
+            }), delay);
+        }
+    };
+    function menuInit() {
+        if (document.querySelector(".icon-menu")) {
+            const menuBtn = document.querySelector(".icon-menu");
+            const menu = document.querySelector(".menu");
+            document.addEventListener("click", (function(e) {
+                if (bodyLockStatus && e.target.closest(".icon-menu")) {
+                    bodyLockToggle();
+                    menuBtn.classList.toggle("active");
+                    menu.classList.toggle("active");
+                }
+            }));
+        }
+    }
+    let addWindowScrollEvent = false;
+    setTimeout((() => {
+        if (addWindowScrollEvent) {
+            let windowScroll = new Event("windowScroll");
+            window.addEventListener("scroll", (function(e) {
+                document.dispatchEvent(windowScroll);
+            }));
+        }
+    }), 0);
+    menuInit();
+    const header = document.querySelector(".header");
+    window.addEventListener("scroll", (() => {
+        const scrollPosition = window.scrollY;
+        if (scrollPosition >= 10) header.classList.add("scroll"); else header.classList.remove("scroll");
+    }));
+    let script_slideUp = (target, duration = 500) => {
+        if (!target.classList.contains("_slide")) {
+            target.classList.add("_slide");
+            target.style.transitionProperty = "height, margin, padding";
+            target.style.transitionDuration = duration + "ms";
+            target.style.height = target.offsetHeight + "px";
+            target.offsetHeight;
+            target.style.overflow = "hidden";
+            target.style.height = 0;
+            target.style.paddingTop = 0;
+            target.style.paddingBottom = 0;
+            target.style.marginTop = 0;
+            target.style.marginBottom = 0;
+            window.setTimeout((() => {
+                target.hidden = true;
+                target.style.removeProperty("height");
+                target.style.removeProperty("padding-top");
+                target.style.removeProperty("padding-bottom");
+                target.style.removeProperty("margin-top");
+                target.style.removeProperty("margin-bottom");
+                target.style.removeProperty("overflow");
+                target.style.removeProperty("transition-duration");
+                target.style.removeProperty("transition-property");
+                target.classList.remove("_slide");
+            }), duration);
+        }
+    };
+    let script_slideDown = (target, duration = 500) => {
+        if (!target.classList.contains("_slide")) {
+            target.classList.add("_slide");
+            if (target.hidden) target.hidden = false;
+            let height = target.offsetHeight;
+            target.style.overflow = "hidden";
+            target.style.height = 0;
+            target.style.paddingTop = 0;
+            target.style.paddingBottom = 0;
+            target.style.marginTop = 0;
+            target.style.marginBottom = 0;
+            target.offsetHeight;
+            target.style.transitionProperty = "height, margin, padding";
+            target.style.transitionDuration = duration + "ms";
+            target.style.height = height + "px";
+            target.style.removeProperty("padding-top");
+            target.style.removeProperty("padding-bottom");
+            target.style.removeProperty("margin-top");
+            target.style.removeProperty("margin-bottom");
+            window.setTimeout((() => {
+                target.style.removeProperty("height");
+                target.style.removeProperty("overflow");
+                target.style.removeProperty("transition-duration");
+                target.style.removeProperty("transition-property");
+                target.classList.remove("_slide");
+            }), duration);
+        }
+    };
+    let script_slideToggle = (target, duration = 500) => {
+        if (target.hidden) return script_slideDown(target, duration); else return script_slideUp(target, duration);
+    };
+    const spoller = document.querySelector(".spoller");
+    if (window.innerWidth < 745) {
+        script_slideUp(spoller.nextElementSibling, 0);
+        const spollerIcon = document.querySelector(".spoller-icon");
+        spollerIcon.addEventListener("click", (() => {
+            script_slideToggle(spoller.nextElementSibling, 500);
+            spollerIcon.classList.toggle("active");
+        }));
+    }
+    window["FLS"] = true;
+})();
